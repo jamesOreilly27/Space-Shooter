67f1e35edcb24710e06c1f95ef5835fd3d733bda
James O'Reilly <James.Oreilly926@gmail.com>
2018-09-26 15:48:19 -0400

    add shoot method to Player Class to allow user to shoot with space bar

--- src/Scenes/Battlefield.js
+++ src/Scenes/Battlefield.js
@@ -10,16 +10,21 @@ export default class Battlefield extends Scene {
 
   preload() {
     this.load.image('player', './assets/playerShip1_green.png')
+    this.load.image('laser', './assets/laserGreen03.png')
   }
 
   create() {
+    //Filling in Battlefield Properties
     this.player = new Player(this.physics.add.sprite(100, 450, 'player')) 
     this.cursors = this.input.keyboard.createCursorKeys()
 
     this.player.sprite.setCollideWorldBounds(true)
+
+    console.log(this.player)
   }
 
   update() {
-    this.player.movement(this.cursors)
+    this.player.move(this.cursors)
+    this.player.shoot(this, 'laser')
   }
 }
--- src/Sprites/Player.js
+++ src/Sprites/Player.js
@@ -3,9 +3,10 @@ import Phaser, { GameObjects } from 'phaser'
 export default class Player {
   constructor(sprite) {
     this.sprite = sprite
+    this.laser = {}
   }
 
-  movement(cursors) {
+  move(cursors) {
     const sprite = this.sprite
 
     if(cursors.left.isDown) sprite.setVelocityX(-250)
@@ -15,5 +16,14 @@ export default class Player {
     if(cursors.down.isDown) sprite.setVelocityY(250)
     else if(cursors.up.isDown) sprite.setVelocityY(-250)
     else sprite.setVelocityY(0)
+  }
+
+  shoot(scene, spriteString) {
+    const spacebar = scene.cursors.space
+    const interval = Math.floor( (scene.time.now - spacebar.timeDown) / 10) * 10
+    if(spacebar.isDown && interval % 150 === 0) {
+      this.laser = scene.physics.add.sprite(this.sprite.x, this.sprite.y - 60, spriteString)
+      this.laser.setVelocityY(-750)
+    }
   }
 }
