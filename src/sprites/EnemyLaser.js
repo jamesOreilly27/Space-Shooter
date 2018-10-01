69712897a5c4b8092b6cc97253ea5c94850bb489
James O'Reilly <James.Oreilly926@gmail.com>
2018-10-01 16:10:33 -0400

    add scale to Laser class constructor

--- src/sprites/EnemyLaser.js
+++ src/sprites/EnemyLaser.js
@@ -3,8 +3,6 @@ import { Laser } from '../sprites'
 export default class EnemyLaser extends Laser {
   constructor(config) {
     super(config)
-    this.scaleX = .6
-    this.scaleY = .6
   }
 
   update() {
