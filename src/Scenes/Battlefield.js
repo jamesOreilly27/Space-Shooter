6d976cbe9fa80bd7e6946527ce9e790332c65d6e
James O'Reilly <James.Oreilly926@gmail.com>
2018-10-01 10:34:36 -0400

    condense the forEach call in update method of the Battlefield scene

--- src/Scenes/Battlefield.js
+++ src/Scenes/Battlefield.js
@@ -40,9 +40,7 @@ export default class Battlefield extends Scene {
   update() {
     this.updateCount++
     this.player.update()
-    this.enemies.children.entries.forEach(enemy => {
-      enemy.update()
-    })
+    this.enemies.children.entries.forEach(enemy => { enemy.update() })
     if(this.updateCount >= 200) this.updateCount = 0
     if(!this.updateCount % 100 && this.enemies.children.entries.length < 6) this.addEnemy()
   }
