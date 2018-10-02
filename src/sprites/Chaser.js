3265ba77913e907275b7e20de71913d07bb6291a
James O'Reilly <James.Oreilly926@gmail.com>
2018-10-02 12:18:51 -0400

    create file for Chaser class

---
+++ src/sprites/Chaser.js
@@ -0,0 +1,19 @@
+import { Ship } from '../sprites'
+
+export default class Chaser extends Ship {
+  constructor(config) {
+    super(config)
+  }
+
+  move() {
+
+  }
+
+  shoot() {
+
+  }
+
+  update() {
+    super.update()
+  }
+}
