5ed5305773fcad79c57315f432e458e1d1372977
James O'Reilly <James.Oreilly926@gmail.com>
2018-10-02 11:22:30 -0400

    remove comment from the boilerplate used to start this project in src/scenes/index

--- src/Scenes/index.js
+++ src/Scenes/index.js
@@ -1,4 +1,2 @@
-//Use this file to funnel Scene exports. This way all imported scenes will be taken from './Scenes'
-
 export { default as Background } from './Background'
 export { default as Battlefield } from './Battlefield'
