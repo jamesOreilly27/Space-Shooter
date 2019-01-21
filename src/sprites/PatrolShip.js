309365cf145efc490368291e51b27f6b892b368c
James O'Reilly <James.Oreilly926@gmail.com>
2019-01-20 22:23:43 -0500

    update move method of PatrolShip Sprite to allow for spawning at any x coordinate

--- src/sprites/PatrolShip.js
+++ src/sprites/PatrolShip.js
@@ -4,10 +4,15 @@ import { Ship, PatrolShipLaser } from '../sprites'
 export default class PatrolShip extends Ship {
   constructor(config) {
     super(config)
+    this.movestarted = false
   }
 
   move() {
     if(this.active) {
+      while(!this.started) {
+        this.body.setVelocityX(80)
+        this.started = true
+      }
       if(this.x > 800) this.body.setVelocityX(-80)
       else if(this.x < 10) this.body.setVelocityX(80)
     }
