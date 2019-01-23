4ba1c2043d7dc56be4528719bc1f2630b8c8b810
James O'Reilly <James.Oreilly926@gmail.com>
2019-01-23 00:29:10 -0500

    change to have individual scoring for each different type of enemy

--- src/sprites/Ship.js
+++ src/sprites/Ship.js
@@ -5,6 +5,7 @@ export default class Ship extends Phaser.GameObjects.Sprite {
     super(config.scene, config.x, config.y, config.key, config.path)
     this.scene.physics.world.enable(this)
     this.scene.add.existing(this)
+    this.key = config.key
     this.scaleX = .4
     this.scaleY = .4
   }
@@ -16,7 +17,10 @@ export default class Ship extends Phaser.GameObjects.Sprite {
   }
 
   updateScore() {
-    this.scene.score += 3
+    if(this.key === 'divebomber') this.scene.score += 5
+    if(this.key === 'fighter') this.scene.score +=2
+    if(this.key === 'chaser') this.scene.score += 3
+    if(this.key === 'patrol-ship') this.scene.score += 3
     this.scene.scoreText.setText(`score: ${this.scene.score}`)
   }
 
