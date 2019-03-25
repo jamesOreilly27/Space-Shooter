7eaa9343a7d33dcc238b1f1742f9f098c52fc54c
James O'Reilly <James.Oreilly926@gmail.com>
2019-03-25 12:30:28 -0400

    create Title scene and export it through scenes/index.js

---
+++ src/scenes/Title.js
@@ -0,0 +1,19 @@
+import Phaser, { Scene } from 'phaser'
+
+export default class Title extends Scene {
+  constructor() {
+    super({ key: 'Title' })
+  }
+
+  preload() {
+
+  }
+
+  create() {
+
+  }
+
+  update() {
+
+  }
+}
