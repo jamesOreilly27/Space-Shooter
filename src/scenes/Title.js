29181fabe92716d17ddacc5f2a623672f11a010c
James O'Reilly <James.Oreilly926@gmail.com>
2019-03-25 13:22:12 -0400

    finish sizing and positioning the title screen images

--- src/scenes/Title.js
+++ src/scenes/Title.js
@@ -6,12 +6,15 @@ export default class Title extends Scene {
   }
 
   preload() {
-    this.load.image('title-image', './assets/playerShip1_green.png')
+    this.load.image('player-title', './assets/playerShip1_green.png')
+    this.load.image('enemy-title', './assets/enemyRed2.png')
+    this.load.image('player-laser', './assets/laserGreen10.png')
   }
 
   create() {
-    this.titleImage = this.add.image(400, 400, 'title-image')
-    console.log(this.titleImage)
+    this.playerTitle = this.add.image(475, 225, 'player-title').setScale(1.3, 1.3).setAngle(225)
+    this.enemyTitle = this.add.image(300, 380, 'enemy-title').setScale(1.15, 1.15).setAngle(45)
+    this.enemyLaser = this.add.image(375, 325, 'player-laser').setAngle(225)
   }
 
   update() {
