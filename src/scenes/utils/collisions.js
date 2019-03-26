65d226ca9c996c7c692077a966b86def95b59a11
James O'Reilly <James.Oreilly926@gmail.com>
2019-03-26 01:01:37 -0400

    move ultility functions used for handling collisions to new file scenes/utils/collisions.js

---
+++ src/scenes/utils/collisions.js
@@ -0,0 +1,34 @@
+export const destroy = (object1, object2) => {
+  object1.destroy()
+  object2.destroy()
+}
+
+export const enemyDestroy = (ship, laser) => {
+  ship.enemyExplode()
+  laser.destroy()
+}
+
+export const playerDestroy = (player, enemyLaser) => {
+  player.playerExplode()
+  enemyLaser.destroy()
+}
+
+export const shieldBlock = (enemy, shield) => {
+  shield.handleCollision()
+  destroy(enemy, shield)
+}
+
+export const laserCollision = (playerLaser, enemyLaser) => {
+  enemyLaser.destroy()
+  playerLaser.body.setVelocityY(-600)
+}
+
+export const meteorDestroy = (laser, meteor) => {
+  laser.destroy()
+  meteor.drop()
+}
+
+export const powerup = (player, powerup) => {
+  powerup.handleCollision()
+  powerup.destroy()
+}
