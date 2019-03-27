78445da6107253d245ae06017884e1aed0b57cd9
James O'Reilly <James.Oreilly926@gmail.com>
2019-03-27 12:41:19 -0400

    update fireRate property on the Player class

--- src/Sprites/Player.js
+++ src/Sprites/Player.js
@@ -8,7 +8,7 @@ export default class Player extends Ship {
     this.laserLevel = 1
     this.speed = 150
     this.nextFire = 0
-    this.fireRate = 1000
+    this.fireRate = 750
     this.body.setCollideWorldBounds(true)
   }
 
