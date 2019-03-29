50fbce87d726a2bc64756f8dd34e58e7bd7b2f1b
James O'Reilly <James.Oreilly926@gmail.com>
2019-03-29 18:48:40 -0400

    write dropShield and dropBomb functions in scenes/utils/itemDrop.js

--- src/scenes/utils/itemDrop.js
+++ src/scenes/utils/itemDrop.js
@@ -1,4 +1,5 @@
 import { genRandNum } from './enemies.js'
+import { ShieldPowerup, Bomb } from '../../sprites/index.js';
 
 //Helper that takes a number a floor and a cieling as arguments
 //returns true if the number falls between the floor and cieling, bottom inclusive
@@ -14,16 +15,25 @@ export const shouldDrop = scene => genRandNum(1000) < 200 && noPowerupOnScreen(s
 export const canUpgradeShield = shieldLevel => shieldLevel < 3
 
 //Helper that determines which ShieldPowerup sprite to render
-export const getShieldPowerupSprite = player => {
-  if(player.shieldLevel === 0) return 'bronze-shield'
-  else if(player.shieldLevel === 1) return 'silver-shield'
+export const getShieldPowerupSprite = shieldLevel => {
+  if(shieldLevel === 0) return 'bronze-shield'
+  else if(shieldLevel === 1) return 'silver-shield'
   else return 'gold-shield'
+}
+
+export const dropShield = ship => {
+  const shieldKey = getShieldPowerupSprite(ship.scene.player.shieldLevel)
+  ship.scene.powerups.add(new ShieldPowerup({ scene: ship.scene, x: ship.x, y: ship.y, key: shieldKey}))
+}
+
+export const dropBomb = ship => {
+  ship.scene.powerups.add(new Bomb({ scene: ship.scene, x: ship.x, y: ship.y, key: 'bomb'}))
 }
 
 //Helper function that generates shield powerup and bomb powerup at a 2:1 rate
 export const choosePowerup = ship => {
   const randNum = genRandNum(900)
   const shieldLevel = ship.scene.player.shieldLevel
-  if(checkRange(randNum, 300, 900) && canUpgradeShield(shieldLevel)) { ship.dropShield()}
-  else { ship.dropBomb() }
+  if(checkRange(randNum, 300, 900) && canUpgradeShield(shieldLevel)) { dropShield(ship)}
+  else { dropBomb(ship) }
 }
