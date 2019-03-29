92f69a0fcfafcb1859dcf7ef8da2c3c6e1b7a34f
James O'Reilly <James.Oreilly926@gmail.com>
2019-03-29 17:37:49 -0400

    add shouldDrop function to scenes/utils/itemDrop.js

--- src/scenes/utils/itemDrop.js
+++ src/scenes/utils/itemDrop.js
@@ -2,3 +2,7 @@ import { genRandNum } from './enemies.js'
 
 //Helper that returns true if no powerups are on screen
 const noPowerupOnScreen = scene => !scene.powerups.children.entries.length
+
+//Helper that returns true 20% of the time if no power ups are on screen
+const shouldDrop = scene => genRandNum(1000) < 200 && noPowerupOnScreen(scene)
+
