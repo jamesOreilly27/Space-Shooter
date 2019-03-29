982a4b7feb492447c8b6cbad42c80a63c670ac72
James O'Reilly <James.Oreilly926@gmail.com>
2019-03-29 19:53:34 -0400

    change choosePowerup function to only drop something if the enemy is a fighter or a divebomber

--- src/scenes/utils/itemDrop.js
+++ src/scenes/utils/itemDrop.js
@@ -9,7 +9,7 @@ export const checkRange = (number, floor, cieling) => number >= floor && number 
 export const noPowerupOnScreen = scene => !scene.powerups.children.entries.length
 
 //Helper that returns true 20% of the time if no power ups are on screen
-export const shouldDrop = scene => genRandNum(1000) < 200 && noPowerupOnScreen(scene)
+export const shouldDrop = scene => genRandNum(1000) < 1000 && noPowerupOnScreen(scene)
 
 //Helper that returns true if the player's shield can upgrade
 export const canUpgradeShield = shieldLevel => shieldLevel < 3
@@ -30,8 +30,9 @@ export const dropBomb = ship => {
   ship.scene.powerups.add(new Bomb({ scene: ship.scene, x: ship.x, y: ship.y, key: 'bomb'}))
 }
 
-//Helper function that generates shield powerup and bomb powerup at a 2:1 rate
+//Helper function that generates shield powerup and bomb powerup at a 2:1 ratio
 export const choosePowerup = ship => {
+  if(ship.key !== 'fighter' && ship.key !== 'divebomber') { return }
   const randNum = genRandNum(900)
   const shieldLevel = ship.scene.player.shieldLevel
   if(checkRange(randNum, 300, 900) && canUpgradeShield(shieldLevel)) { dropShield(ship)}
