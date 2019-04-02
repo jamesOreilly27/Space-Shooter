fbfbd5e3283cc46fdc8795f04ae69726fc281a3a
James O'Reilly <James.Oreilly926@gmail.com>
2019-04-01 23:29:12 -0400

    add the addRectangle method to the UpgradeShop scene

--- src/scenes/UpgradeShop.js
+++ src/scenes/UpgradeShop.js
@@ -13,6 +13,10 @@ export default class UpgradeShop extends Scene {
     this.load.image('upgrade-btn', './assets/upgrade-btn.png')
   }
 
+  addRectangle(x, y) {
+    return new UpgradeContainer({ scene: this, x: x, y: y, width: 216, height: 300, fillColor: 0x000036, alpha: .8 })
+  }
+
   create() {
 
   }
