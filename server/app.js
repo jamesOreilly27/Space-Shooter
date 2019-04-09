3aa5661b0846aa8e997fc8dcf2cf4efa16090499
James O'Reilly <James.Oreilly926@gmail.com>
2019-04-09 19:09:23 -0400

    test app.js

--- server/app.js
+++ server/app.js
@@ -13,6 +13,6 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, '..', 'public')))
 app.use('./static', express.static(path.join(__dirname, 'public')))
 
-// app.get('*', (req, res, next) => res.sendFile(path.join(__dirname, '..', 'public', 'index.html')))
+app.get('*', (req, res, next) => res.sendFile(path.join(__dirname, '..', 'public', 'index.html')))
 
 app.listen(PORT, () => console.log(chalk.blue.bgWhite.bold(`We'll Do It Live on Port ${PORT}`)))
